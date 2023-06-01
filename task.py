d4d25cb839716700ef907b30c1ce5b767981d5c5
Alwin Nocom <alwinnocom@hotmail.com>
2023-05-31 22:15:17 -0700

    Test commit for group repository

---
+++ task.py
@@ -0,0 +1,17 @@
+
+# Function 1
+def conv_num(num_str):
+    """  """
+    pass
+
+
+# Function 2
+def my_datetime(num_sec):
+    """  """
+    pass
+
+
+# Function 3
+def conv_endian(num, endian='big'):
+    """  """
+    pass
